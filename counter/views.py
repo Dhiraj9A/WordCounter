3b891058154a3c0a7aece1c6c69ccf226121f2df
Dhiraj9k <dk2141487@gmail.com>
2023-04-15 20:57:49 +0530

    WordCounter project complited

---
+++ counter/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+def result(request):
+    name=request.GET['username'] 
+    ls=len(name)
+    word=len(name.split())
+    val=['a','e','i','o','u']
+    val_count = 0
+    consonatcount = 0
+    for i in name:
+        if i.isalpha():
+            if i in val or i.lower() in val:
+                val_count+=1
+            else:
+                consonatcount+=1
+    data={'name':name,'ls':ls,'word':word,'val_count':val_count,
+          'consonatcount':consonatcount
+        }
+    return render(request,'index.html',data)
+
+
